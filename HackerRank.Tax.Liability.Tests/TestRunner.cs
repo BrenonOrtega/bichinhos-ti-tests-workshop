d929a704a997a9649025cf76c78c7a6a29c2621c
BrenonOrtega <brenon.ortega@hotmail.com>
2023-07-14 21:56:01 +0200

    Fixes tests assertions and improved Program testability

--- HackerRank.Tax.Liability.Tests/TestRunner.cs
+++ HackerRank.Tax.Liability.Tests/TestRunner.cs
@@ -1,3 +1,6 @@
+using HackerRank.Tax.Liability.App;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
 namespace HackerRank.Tax.Liability;
@@ -5,23 +8,27 @@ namespace HackerRank.Tax.Liability;
 public class TestRunner
 {
     [Theory]
-    [InlineData("Italy", 20.5, 40, 188.60)]
-    [InlineData("Germany", 25, 35, 0)]
-    [InlineData("Ireland", 80, 40, 0)]
+    [InlineData("Italy", 20.5, 40, 240.83)]
+    [InlineData("Germany", 25, 35, 275.05)]
+    [InlineData("Ireland", 80, 40, 1145.54)]
     //Didn t make the calculations by myself for each case, just making sure its passing.
     public async Task Should_Calculate_Based_On_Countries(string country, decimal hourlyRate, int hours, decimal expected)
     {
-        var result = await ConsoleApplication.GetApp().Calculate(country, hourlyRate, hours);
+        var calculator = ConsoleApplication.GetApp();
+        var result = await calculator.Calculate(country, hourlyRate, hours);
 
-        Assert.Equal(expected, result.TotalTaxes);
+        Assert.Equal(expected, result.TotalTaxes, precision: 2);
     }
 
-    [Theory]
-    [InlineData("Italy", 20.5, 40, 188.60)]
-    [InlineData("Germany", 25, 35, 0)]
-    [InlineData("Ireland", 80, 40, 0)]
-    public async Task Application_Should_Run_Succesfully(string country, decimal hourlyRate, int hours, decimal expected)
+    [Fact]
+    public async Task Not_Implemented_Calculator_Should_Throw()
     {
-        await Program.Main(null);
+        var invalidCountry = "bryanland";
+        Program.ConfigureApplication(WebApplication.CreateBuilder());
+        var calculator = Program.ServiceProvider.GetRequiredService<ICalculatorApp>();
+
+        var throwAction = () => calculator.Calculate(invalidCountry, 20, 20);
+
+        await Assert.ThrowsAsync<NotImplementedException>(throwAction);
     }
 }
--- HackerRank.Tax.Liability/Program.cs
+++ HackerRank.Tax.Liability/Program.cs
@@ -6,6 +6,7 @@ namespace HackerRank.Tax.Liability;
 
 public class Program
 {
+    public static IServiceProvider ServiceProvider { get; private set; }
     public static async Task Main(string[] args)
     {
         if (args?.Any(x => x == "console") ?? false)
@@ -18,7 +19,29 @@ public class Program
     private static async Task StartApi(string[]? args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        WebApplication app = ConfigureApplication(builder);
 
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.UseHttpLogging();
+
+
+        app.Run();
+    }
+
+    public static WebApplication ConfigureApplication(WebApplicationBuilder builder)
+    {
         builder.Services.AddMvc(x =>
         {
             x.EnableEndpointRouting = true;
@@ -42,22 +65,9 @@ public class Program
         builder.Services.AddCalculatorServices();
 
         var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        app.UseHttpLogging();
-
-        app.Run();
+        
+        ServiceProvider = app.Services;
+        
+        return app;
     }
 }
